39f7ae07f04a76b7a93dc8089db2a9cf9e92bf17
stefan12002 <97023030+stefan12002@users.noreply.github.com>
2023-03-07 23:42:37 +0200

    BankManagementSystem

---
+++ Conn.java
@@ -0,0 +1,16 @@
+package bank.management.system;
+import java.sql.*;
+public class Conn {
+    Connection c;
+    Statement s;
+    public Conn(){
+        try{
+
+            c=DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","Nambani2312da!");
+            s=c.createStatement();
+
+        } catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
